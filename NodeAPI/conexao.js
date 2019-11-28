08f1d587755bffaf07ae1f38a513b3e92c0290ce
Trsouza <32604592+Trsouza@users.noreply.github.com>
2019-11-28 16:52:46 -0300

    Update conexao.js

--- NodeAPI/conexao.js
+++ NodeAPI/conexao.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
- user: 'encontros',
-  host: '200.129.62.41',
-  database: 'guardiaodb',
-  password: 'EncDbUserN2s',
+  user: 'postlocal',
+  host: '127.0.0.1',
+  database: 'encontros',
+  password: '123',
   port: 5432,
 })
 	const db_name = "encontrosuniversitarios."
